1871a6b2f298b0a613d8c242a25e583a0307f5f2
Sandis Smans <smanssandis@gmail.com>
2022-11-11 22:57:07 +0200

    Created index.html and script.js file. Implemented calc. functions
    Added boilerplate in index.html. Removed remainder "feature" in
    README.md.
    
    Implemented basic calculator operations in script.js

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ It can be interacted with using a mouse.
 
 ## Features
 
-* Basic operations are supported (divide, multiple, add, subtract, remainder)
+* Basic operations are supported (divide, multiple, add, subtract)
 * Stores the last operation calculated on top of the calculator screen.
 * Can __clear/delete__ a single input OR __clear__ the whole display.
 
---
+++ script.js
@@ -0,0 +1,28 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+} 
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+// Recieves an operator and numbers that calls the required function
+const operate = (operator, num1, num2) => {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    }
+}
