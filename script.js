d19e8058c666f30f1e0b00348b975305b86af54f
Sandis Smans <smanssandis@gmail.com>
2022-11-14 20:31:03 +0200

    Created calculation logic
    - Added all operator logic, decimal calculation not implemented
    - deleting all and undo
    - previous calculation is stored on top of the screen

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const add = (num1, num2) => {
     return num1 + num2;
-} 
+}
 
 const subtract = (num1, num2) => {
     return num1 - num2;
@@ -16,13 +16,96 @@ const divide = (num1, num2) => {
 
 // Recieves an operator and numbers that calls the required function
 const operate = (operator, num1, num2) => {
-    if (operator === "+") {
-        return add(num1, num2);
-    } else if (operator === "-") {
-        return subtract(num1, num2);
-    } else if (operator === "*") {
-        return multiply(num1, num2);
-    } else if (operator === "/") {
-        return divide(num1, num2);
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "ERROR";
     }
 }
+
+// Gets all buttons from HTML
+const buttons = [...document.querySelectorAll("button")];
+
+// Gets main screen from HTML
+let displayValue = document.getElementById("main-screen");
+let historyValue = document.getElementById("history-screen");
+
+const numberRegex = /^[0-9]/;
+const deleteAllRegex = /^[A]/;
+const undoRegex = /^[C]/;
+const operatorRegex = /[+\-/*]/;
+const equalsRegex = /[=]/;
+
+let firstOperand = 0;
+let secondOperand = 0;
+let currentOperator = "+";
+
+// Gets the clicked buttons value and does the required operations on it
+const handleValue = (event) => {
+    const value = event.target.innerText;
+
+    if (numberRegex.test(value)) {
+        // Displays pressed button value(number) on screen
+        displayNumber(value);
+    } else if (deleteAllRegex.test(value)) {
+        // Deletes all input on screen
+        deleteAll();
+    } else if (undoRegex.test(value)) {
+        // Deletes the last input
+        undoInput();
+    } else if (operatorRegex.test(value)) {
+        // Displays operator to the screen
+        displayOperator(value);
+    } else if (equalsRegex.test(value)) {
+        calculate();
+    }
+}
+
+const displayNumber = (number) => {
+    if (displayValue.innerText.length <= 12) {
+        displayValue.innerText += number;
+    }
+}
+
+const deleteAll = () => {
+    displayValue.innerText = "";
+}
+
+const undoInput = () => {
+    displayValue.innerText = displayValue.innerText.substring(0, displayValue.innerText.length - 1);
+}
+
+const displayOperator = (operator) => {
+    if (!operatorRegex.test(displayValue.innerText)) {
+        firstOperand = displayValue.innerText;
+        currentOperator = operator;
+        displayValue.innerText += operator;
+    } else {
+        calculate();
+        firstOperand = displayValue.innerText;
+        currentOperator = operator;
+        displayValue.innerText += operator;
+    }
+}
+
+const calculate = () => {
+    secondOperand = displayValue.innerText.substring(displayValue.innerText.indexOf(currentOperator) + 1);
+    const result = operate(currentOperator, firstOperand, secondOperand);
+    historyValue.innerText = displayValue.innerText;
+    displayValue.innerText = result;
+}
+
+// Ads a event for each button click
+buttons.forEach(button => {
+    button.addEventListener("click", handleValue);
+})
